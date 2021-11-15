d11d74318eae35cd0523eeb87e76117b9d5b7e70
BihariBalazs <bihari.b16@gmail.com>
2021-11-15 19:02:41 +0100

    ADD: creat inventory

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -1,10 +1,19 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.items.Item;
+
+import java.util.ArrayList;
 
 public class Player extends Actor {
+    private ArrayList<Item> inventory = new ArrayList<>();
+
     public Player(Cell cell) {
         super(cell);
+    }
+
+    public void setInventory(Item item){
+        inventory.add(item);
     }
 
     public String getTileName() {
