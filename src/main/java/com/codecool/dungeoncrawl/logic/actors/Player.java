5994cfad2b43871406b031ec547773cada406573
barna <barna.m42@gmail.com>
2021-11-16 12:45:43 +0100

    ADD: Doors for the dungeon

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -6,6 +6,7 @@ import com.codecool.dungeoncrawl.logic.items.Item;
 import java.util.ArrayList;
 
 public class Player extends Actor {
+    private String route;
     private ArrayList<Item> inventory = new ArrayList<>();
 
     public Player(Cell cell) {
@@ -20,6 +21,14 @@ public class Player extends Actor {
         if (getCell().getItem() != null){
             setInventory(getCell().getItem());
         }
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    public String getRoute() {
+        return route;
     }
 
     public String getTileName() {
