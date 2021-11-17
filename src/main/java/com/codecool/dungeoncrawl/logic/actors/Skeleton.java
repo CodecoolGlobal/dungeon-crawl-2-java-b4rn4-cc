deed2fc6609cf80c5fc96d4bd81955c8c5d23f08
BlaskoMate <blaskomate3@gmail.com>
2021-11-17 20:21:53 +0100

    FEAT: add circular movement to Skeleton

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -1,11 +1,16 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.Direction;
 import com.codecool.dungeoncrawl.logic.GameMap;
 
 public class Skeleton extends Monster {
+    private int directionIndex;
+
     public Skeleton(Cell cell) {
         super(cell, 10, 1, 2, 5);
+        int index = r.nextInt(8);
+        setValidIndex(index-2);
     }
 
 
@@ -15,8 +20,21 @@ public class Skeleton extends Monster {
     }
 
     public void act(GameMap map, int index){
-        act2(map, index);
+        MonsterAct(map, index);
     }
 
+    @Override
+    protected void SetAlternativeDir() {
+        setValidIndex(directionIndex + 1);
+        direction = Direction.values()[directionIndex / 2];
+    }
 
+    private void setValidIndex(int index){
+        if (index < 0){
+            index++;
+        } else if (index == Direction.values().length * 2 - 2){
+            index = 0;
+        }
+        directionIndex = index;
+    }
 }
