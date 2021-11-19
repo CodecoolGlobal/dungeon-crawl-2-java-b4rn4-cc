9bdc893321cedaeba8c154b1c9f0311398686fa5
BlaskoMate <blaskomate3@gmail.com>
2021-11-19 03:52:16 +0100

    FEAT: add Freeze.java

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Freeze.java
@@ -0,0 +1,14 @@
+package com.codecool.dungeoncrawl.logic.items;
+
+import com.codecool.dungeoncrawl.logic.Cell;
+
+public class Freeze extends Item{
+    public Freeze(Cell cell) {
+        super(cell, true);
+    }
+
+    @Override
+    public String getTileName() {
+        return "Freeze spell";
+    }
+}
