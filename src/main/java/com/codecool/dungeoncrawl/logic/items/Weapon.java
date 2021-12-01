d595760b665edaac02217c0f8ae10bd90a9e9f01
BihariBalazs <bihari.b16@gmail.com>
2021-12-01 15:24:48 +0100

    MODIFY: add cell field to weapon

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Weapon.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Weapon.java
@@ -6,12 +6,14 @@ public class Weapon extends Item{
     private final int damage;
     private final int crit;
     private final String name;
+    private Cell cell;
 
     public Weapon(Cell cell, String name, int damage, int crit){
         super(cell, true);
         this.damage = damage;
         this.crit = crit;
         this.name = name;
+        this.cell = cell;
     }
 
     @Override
@@ -25,5 +27,14 @@ public class Weapon extends Item{
 
     public int getCrit() {
         return crit;
+    }
+
+    @Override
+    public Cell getCell() {
+        return cell;
+    }
+
+    public String getName(){
+        return name;
     }
 }
