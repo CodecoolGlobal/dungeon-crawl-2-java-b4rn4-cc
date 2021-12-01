2f475d1e4f8d1b0c5a191235ec31c4ffb896d462
BlaskoMate <blaskomate3@gmail.com>
2021-12-01 15:04:27 +0100

    FEAT: add Map Jdbc

---
+++ src/main/java/com/codecool/dungeoncrawl/dao/MapDao.java
@@ -0,0 +1,13 @@
+package com.codecool.dungeoncrawl.dao;
+
+import com.codecool.dungeoncrawl.model.MapModel;
+
+import java.util.List;
+
+public interface MapDao {
+    void add(MapModel map);
+    void update(MapModel map);
+    MapModel get(int id);
+    List<MapModel> getAll();
+}
+
