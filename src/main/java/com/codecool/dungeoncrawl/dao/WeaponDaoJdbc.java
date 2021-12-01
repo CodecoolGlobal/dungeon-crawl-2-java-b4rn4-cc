a7cd8e4b78f827b33116c165443f085b5157f5c0
BihariBalazs <bihari.b16@gmail.com>
2021-12-01 19:42:31 +0100

    MODIFY: modify the methods with the new fields

--- src/main/java/com/codecool/dungeoncrawl/dao/WeaponDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/WeaponDaoJdbc.java
@@ -17,13 +17,15 @@ public class WeaponDaoJdbc implements WeaponDao{
     @Override
     public void add(WeaponModel weapon) {
         try (Connection conn = dataSource.getConnection()){
-            String sql = "INSERT INTO weapon (x, y, damage, crit, name, map_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO weapon (x, y, damage, crit, name, inventory_id, map_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            st.setInt(2, weapon.getX());
-            st.setInt(3, weapon.getY());
-            st.setInt(4, weapon.getDamage());
-            st.setInt(5, weapon.getCrit());
-            st.setString(6, weapon.getName());
+            st.setInt(1, weapon.getX());
+            st.setInt(2, weapon.getY());
+            st.setInt(3, weapon.getDamage());
+            st.setInt(4, weapon.getCrit());
+            st.setString(5, weapon.getName());
+            st.setInt(6, weapon.getInventoryId());
+            st.setInt(7, weapon.getMapId());
             st.executeUpdate();
             ResultSet rs = st.getGeneratedKeys();
             rs.next();
@@ -36,10 +38,15 @@ public class WeaponDaoJdbc implements WeaponDao{
     @Override
     public void update(WeaponModel weapon) {
         try (Connection conn = dataSource.getConnection()) {
-            String sql = "UPDATE weapon SET x = ?, y = ? WHERE id = ?";
+            String sql = "UPDATE weapon SET x = ?, y = ?,  damage = ?, crit = ?, inventory_id = ?, map_id = ? WHERE id = ?";
             PreparedStatement st = conn.prepareStatement(sql);
             st.setInt(1, weapon.getX());
             st.setInt(2, weapon.getY());
+            st.setInt(3, weapon.getDamage());
+            st.setInt(4, weapon.getCrit());
+            st.setString(5, weapon.getName());
+            st.setInt(6, weapon.getInventoryId());
+            st.setInt(7, weapon.getMapId());
             st.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -49,14 +56,15 @@ public class WeaponDaoJdbc implements WeaponDao{
     @Override
     public WeaponModel get(int id) {
         try (Connection conn = dataSource.getConnection()) {
-            String sql = "SELECT x, y, damage, crit, name FROM weapon WHERE id = ?";
+            String sql = "SELECT x, y, damage, crit, name, inventory_id, map_id FROM weapon WHERE id = ?";
             PreparedStatement st = conn.prepareStatement(sql);
             st.setInt(1, id);
             ResultSet rs = st.executeQuery();
             if(!rs.next()){
                 return null;
             }
-            WeaponModel weapon = new WeaponModel(rs.getInt(1), rs.getInt(2), rs.getString(5), rs.getInt(3), rs.getInt(4));
+            WeaponModel weapon = new WeaponModel(rs.getInt(1), rs.getInt(2), rs.getString(5),
+                    rs.getInt(3), rs.getInt(4), rs.getInt(6), rs.getInt(7));
             return weapon;
         } catch (SQLException e) {
             throw new RuntimeException(e);
