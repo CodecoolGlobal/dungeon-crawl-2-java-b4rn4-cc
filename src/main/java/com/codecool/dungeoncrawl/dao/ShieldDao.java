8e94028757d39f487b26e1e685d85375801b9f29
BihariBalazs <bihari.b16@gmail.com>
2021-12-01 18:18:51 +0100

    MODIFY: modify shield object to shield model object

--- src/main/java/com/codecool/dungeoncrawl/dao/ShieldDao.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/ShieldDao.java
@@ -2,15 +2,16 @@ package com.codecool.dungeoncrawl.dao;
 
 import com.codecool.dungeoncrawl.logic.items.Shield;
 import com.codecool.dungeoncrawl.logic.items.Weapon;
+import com.codecool.dungeoncrawl.model.ShieldModel;
 
 import java.util.List;
 
 public interface ShieldDao {
-    public void add(Shield shield );
+    public void add(ShieldModel shield );
 
-    public void update(Shield shield );
+    public void update(ShieldModel shield );
 
-    public Shield get(int id);
+    public ShieldModel get(int id);
 
-    public List<Weapon> getAll();
+    public List<ShieldModel> getAll();
 }
