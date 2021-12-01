24c0b215d7c647c3449e6a58b1c51134f6402541
barna <barna.m42@gmail.com>
2021-12-01 13:29:57 +0100

    FIX: database schema because circular reference

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -11,8 +11,8 @@ CREATE TABLE public.game_state (
     id serial NOT NULL PRIMARY KEY,
     game_state_name varchar NOT NULL,
     current_map_number integer NOT NULL,
-    saved_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    player_id integer NOT NULL -- FK +
+    saved_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+--     player_id integer NOT NULL -- FK +
 );
 
 CREATE TABLE public.player (
@@ -21,7 +21,8 @@ CREATE TABLE public.player (
    hp integer NOT NULL,
    x integer NOT NULL,
    y integer NOT NULL,
-   inventory_id integer NOT NULL --FK
+--    inventory_id integer NOT NULL, --FK
+   game_state_id integer NOT NULL -- FK
 );
 
 CREATE TABLE public.inventory (
@@ -30,7 +31,8 @@ CREATE TABLE public.inventory (
    health_potion integer,
    freeze_potion integer,
    weapon_id integer, -- FK
-   shield_id integer -- FK
+   shield_id integer, -- FK
+    player_id integer NOT NULL -- FK
 );
 
 CREATE TABLE public.maps (
@@ -75,16 +77,17 @@ CREATE TABLE public.door (
     map_id integer -- FK
 );
 
-ALTER TABLE ONLY public.game_state
-    ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
+-- ALTER TABLE ONLY public.game_state
+--     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
 
 ALTER TABLE ONLY public.player
-    ADD CONSTRAINT fk_inventory_id FOREIGN KEY (inventory_id) REFERENCES public.inventory(id);
+--     ADD CONSTRAINT fk_inventory_id FOREIGN KEY (inventory_id) REFERENCES public.inventory(id),
+    ADD CONSTRAINT fk_game_state_id FOREIGN KEY (game_state_id) REFERENCES public.game_state(id);
 
 ALTER TABLE ONLY public.inventory
     ADD CONSTRAINT fk_weapon_id FOREIGN KEY (weapon_id) REFERENCES public.weapon(id),
-    ADD CONSTRAINT fk_shield_id FOREIGN KEY (shield_id) REFERENCES public.shield(id);
-    -- ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
+    ADD CONSTRAINT fk_shield_id FOREIGN KEY (shield_id) REFERENCES public.shield(id),
+    ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
 
 ALTER TABLE ONLY public.maps
     ADD CONSTRAINT fk_game_state_id FOREIGN KEY (game_state_id) REFERENCES public.game_state(id);
