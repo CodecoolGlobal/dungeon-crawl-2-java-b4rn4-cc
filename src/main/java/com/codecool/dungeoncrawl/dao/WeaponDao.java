53f7afa705eaf6e5eefbe605c2b83db08241a36e
BihariBalazs <bihari.b16@gmail.com>
2021-12-01 18:05:08 +0100

    MODIFY: modify weapon object to weapon model object

--- src/main/java/com/codecool/dungeoncrawl/dao/WeaponDao.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/WeaponDao.java
@@ -1,15 +1,15 @@
 package com.codecool.dungeoncrawl.dao;
 
-import com.codecool.dungeoncrawl.logic.items.Weapon;
+import com.codecool.dungeoncrawl.model.WeaponModel;
 
 import java.util.List;
 
 public interface WeaponDao {
-    public void add(Weapon weapon);
+    public void add(WeaponModel weapon);
 
-    public void update(Weapon weapon);
+    public void update(WeaponModel weapon);
 
-    public Weapon get(int id);
+    public WeaponModel get(int id);
 
-    public List<Weapon> getAll();
+    public List<WeaponModel> getAll();
 }
