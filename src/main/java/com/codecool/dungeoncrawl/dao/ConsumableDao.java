2445ffa111e8faa186ab9e378f43f96ac5851208
barna <barna.m42@gmail.com>
2021-12-02 10:05:36 +0100

    ADD: get all items on map

--- src/main/java/com/codecool/dungeoncrawl/dao/ConsumableDao.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/ConsumableDao.java
@@ -3,8 +3,11 @@ package com.codecool.dungeoncrawl.dao;
 
 import com.codecool.dungeoncrawl.model.ConsumableModel;
 
+import java.util.List;
+
 public interface ConsumableDao {
     void add(ConsumableModel state);
     void update(ConsumableModel state);
     ConsumableModel get(int id);
+    List<ConsumableModel> getAll(int mapId);
 }
